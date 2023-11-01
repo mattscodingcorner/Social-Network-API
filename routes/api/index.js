2c620138f43d3e26e9d203794db36f734c399a08
unknown <matt.j.velazquez@gmail.com>
2023-11-01 12:20:38 -0700

    updated routes

---
+++ routes/api/index.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+
+const thoughtRoutes = require('./thoughts');
+const userRoutes = require('./users');
+
+router.use('/thoughts', thoughtRoutes);
+router.use('/users', userRoutes);
+
+module.exports = router;
+
